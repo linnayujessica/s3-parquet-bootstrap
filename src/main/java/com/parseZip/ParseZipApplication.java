44100cbd4efc7010b9b19a2599fb133b87113a7c
linnayujessica <linnayujessica0119@gmail.com>
2021-09-08 16:11:26 +1000

    Initial commit(08/09/2021)

---
+++ src/main/java/com/parseZip/ParseZipApplication.java
@@ -0,0 +1,13 @@
+package com.parseZip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseZipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParseZipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parseZip/ParseZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parseZip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParseZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
